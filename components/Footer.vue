2917d5a1881c3ed9456b4c0f7a61030cfe329fb3
sxoorp <chehamikamalennour@gmail.com>
2024-08-26 12:18:34 +0100

    update: footer

---
+++ components/Footer.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="flex justify-between items-center p-4">
+        <div class="flex items-center gap-2">
+            <p class="text-base font-normal">Made with</p>
+            <UButton icon="i-heroicons-heart-solid" color="red" size="2xs" />
+        </div>
+        <UButton to="https://github.com/sxoorp/shinoko" target="_blank" icon="i-flowbite-github-solid" variant="soft">
+            Github</UButton>
+    </div>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -3,8 +3,10 @@
         <Meta name="description"
             content="Enjoy reading manga through a large collection of high quality content now on shinoko" />
         <Meta name="keywords" content="Shinoko, Shinoko Manga, Manga Shinoko, Read Manga, Browse Manga" />
+        <Style type="text/css" children="::-webkit-scrollbar { width: 0; }"></Style>
     </Head>
     <NuxtLoadingIndicator color="#14B8A6" />
     <Header />
     <slot />
+    <Footer />
 </template>
