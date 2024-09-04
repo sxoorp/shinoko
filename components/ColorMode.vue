57b022a650e1d1de21aff08861c4a2eae9c72adb
sxoorp <chehamikamalennour@gmail.com>
2024-09-04 12:08:21 +0100

    update: color mode switcher

---
+++ components/ColorMode.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const mode = useColorMode();
+const isDark = computed({
+    get() {
+        return mode.value === "dark"
+    },
+    set() {
+        mode.preference = mode.value === "dark" ? "light" : "dark"
+    }
+});
+</script>
+
+<template>
+    <ClientOnly>
+        <UButton :icon="isDark ? 'i-heroicons-moon-solid' : 'i-heroicons-sun-solid'" variant="soft" aria-label="Theme"
+            size="lg" @click="isDark = !isDark" />
+    </ClientOnly>
+</template>
--- components/Header.vue
+++ components/Header.vue
@@ -1,7 +1,10 @@
 <template>
     <UContainer as="div" class="flex justify-between items-center p-4">
         <NuxtLink to="/" class="text-3xl font-extrabold uppercase hover:text-primary">Shinoko</NuxtLink>
-        <UButton icon="i-heroicons-magnifying-glass" variant="soft" size="lg" />
+        <UContainer as="div" class="flex items-center gap-2">
+            <UButton icon="i-heroicons-magnifying-glass" variant="soft" size="lg" />
+            <ColorMode />
+        </UContainer>
     </UContainer>
     <UDivider />
 </template>
